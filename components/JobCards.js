723d8a63d322674708faa37b2de9f2a27d2ffd49
hoichoidev1 <development@hoichoi.tv>
2022-03-14 18:51:35 +0530

    Customization

--- components/JobCards.js
+++ components/JobCards.js
@@ -6,11 +6,6 @@ const JobCards = ({ title, subtitle, active, onClick }) => {
         <div className={styles.title}>{title}</div>
         <div className={styles.row}>
             <div className={styles.subtitle}>{subtitle}</div>
-            <div>
-                <svg width="24" height="24" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" className={active ? `${styles.icon} ${styles.activeIcon}` : styles.icon}>
-                <path d="M9.33333 4.66669L18.6667 14L9.33333 23.3334" stroke={active ? "white" : "#6b717e"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-            </div>
         </div>
     </div>
   )
--- components/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.sbProjectURL
-const supabaseAnonKey = process.env.sbAPIKey
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
